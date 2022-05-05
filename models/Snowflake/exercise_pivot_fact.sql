c2b07004d6c213ac4ebb02fa2c31d8f870ece725
Gary Manley <garymanley@googlemail.com>
2022-05-05 09:32:48 +0000

    Changing to catch all for pivots

--- models/Snowflake/exercise_pivot_fact.sql
+++ models/Snowflake/exercise_pivot_fact.sql
@@ -1,10 +1,15 @@
-{%- set activities = ['Pull_Up', 'Crunch','Push_Up','Plank_Time','Dumbbells','Box_Jump'] -%}
+{%- set activities = dbt_utils.get_column_values(
+    table=ref('stg_exercise_star'),
+    column= 'description'
+) -%}
 
-select date_done,
+SELECT 
+      {{ dbt_utils.current_timestamp() }} last_refresh,
+       date_done,
        {% for activity in activities -%}
 
-       sum(case when replace(description,' ','_') = '{{ activity }}' then act_count else 0 end)
-            as {{ activity }}_count
+       sum(case when description = '{{ activity }}' then act_count else 0 end)
+            as {{ dbt_utils.slugify(activity) }}_count
 
        {%- if not loop.last -%}
          ,
@@ -12,6 +17,6 @@ select date_done,
 
        {%- endfor %}
  
-from {{ source('exercise_data','V_EXERCISE_STAR') }}
+from {{ ref('stg_exercise_star') }}
 
-   group by 1
+   group by 2
