499b667f90246c53236346bb0e347d9a5d0334e6
Gary Manley <garymanley@googlemail.com>
2022-05-07 20:21:03 +0000

    walking data

--- models/Snowflake/stg_walking_fact.sql
+++ models/Snowflake/stg_walking_fact.sql
@@ -1,4 +1,9 @@
 {{ config(materialized='view') }}
 
-select *
-from {{ source('WALKING_DATA','') }}
+select  WALKING_DATE, 
+        STEPS , 
+        DISTANCE as DISTANCE_M , 
+        STEPS/1000 as STEP_K,
+        DISTANCE / 1000 DISTANCE_KM, 
+        ROUND(DISTANCE / 1609,3) DISTANCE_MILES 
+from {{ source('exercise_data','WALKING_DATA') }}
