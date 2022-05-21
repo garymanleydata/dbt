2d07b1119fe22c1cea6840469a6783d24b051638
Gary Manley <garymanley@googlemail.com>
2022-05-21 17:14:43 +0000

    expand dates

--- models/Utils/all_dates.sql
+++ models/Utils/all_dates.sql
@@ -4,7 +4,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="cast('2024-01-01' as date)",
+    start_date="cast('2022-01-01' as date)",
     end_date="cast('2025-01-01' as date)"
    )
 }}
